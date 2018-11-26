483fe19b5dfbf1e73c063e96fa7975d961b294c0
Tacktiik <kaldeo.serveur@gmail.com>
2018-11-26 19:31:58 +0100

    Add files via upload

---
+++ ScreenOfDeath/__resource.lua
@@ -0,0 +1,24 @@
+-- MADE BY KALU AND KASHNAR -- 
+-- 	   EDIT BY TACKTIIK     --
+
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+client_script "main.lua"
+
+ui_page 'html/death.html'
+
+files {
+	'html/death.html',
+	'html/css/death.css',
+	'html/js/death.js',
+	'html/font/OSL.ttf',
+	'html/font/OpenSans-Semibold.ttf',
+	'html/font/OpenSans-Regular.ttf',
+	'html/font/OpenSans-Light.ttf',
+	'html/font/OpenSans-ExtraBold.ttf',
+	'html/font/OpenSans-Bold.ttf'
+}
+
+-- MADE BY KALU AND KASHNAR -- 
+-- 	   EDIT BY TACKTIIK     --
